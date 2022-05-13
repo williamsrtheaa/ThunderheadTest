d63e06f25ba8997718233ed3737aa26c684dc0bf
williamsrtheaa <105377478+williamsrtheaa@users.noreply.github.com>
2022-05-13 15:06:48 +0100

    Update latestDocCheck_Main.ps1

--- latestDocCheck_Main.ps1
+++ latestDocCheck_Main.ps1
@@ -1,7 +1,7 @@
     $startDate = (Get-Date -Day 1)#.AddMonths(-1)
     #$startDate = Get-Date -Day 1 -Hour 0 -Minute 0 -Second 0 -Millisecond 0
     $endDate = ($startDate).AddDays(15)
-    $publicFolder = 'I:\Document Snap Checks'
+    $publicFolder = 'I:\Document Snap Checks'change
     $prodFolder = 'D:\aascripts\ReportingScripts\docSnapCheck\test'
     #$testFolder = 'I:\Document Snap Checks\TEST'
 
