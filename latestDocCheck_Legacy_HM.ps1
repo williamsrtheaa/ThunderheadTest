5805db4323db7ca6e909502e97e0f9c248e444e4
williamsrtheaa <105377478+williamsrtheaa@users.noreply.github.com>
2022-05-13 15:05:33 +0100

    Add files via upload

---
+++ latestDocCheck_Legacy_HM.ps1
@@ -0,0 +1,129 @@
+   ############################################SQL CONNECTION####################################################################
+    $SQLServer = "mlh1aag02"
+    #$SQLServer="MLH1SQLUAT01\THUAT"
+    $SQLDBName = "Thunderhead"
+
+
+    $SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+    $SqlConnection.ConnectionString = "Server = $SQLServer; Database = $SQLDBName;Connection Timeout=160; Integrated Security = SSPI; "
+    $sqlConnection.Open()
+
+    if ($sqlConnection.State -ne [Data.ConnectionState]::Open) 
+    {
+        "Connection to DB is not open."
+        Exit
+    }
+
+    $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+    $SqlCmd.Connection = $sqlConnection
+        
+    $SqlCmd.CommandType = [System.Data.CommandType]::StoredProcedure;
+    $SqlCmd.CommandTimeout=600
+
+    $SqlCmd.CommandText = 'dbo.aa_report_Doc_Snap_Check';
+    
+    $SqlCmd.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@startDate",[Data.SQLDBType]::DateTime))) | Out-Null
+    $SqlCmd.Parameters[0].Value = $startDate;
+    $SqlCmd.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@endDate",[Data.SQLDBType]::DateTime))) | Out-Null
+    $SqlCmd.Parameters[1].Value = $endDate;
+    $SqlCmd.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@batchName",[Data.SQLDBType]::NVARCHAR))) | Out-Null
+    $SqlCmd.Parameters[2].Value = $batchName;
+    $SqlCmd.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@productType",[Data.SQLDBType]::NVARCHAR))) | Out-Null
+    $SqlCmd.Parameters[3].Value = $productType;
+    $SqlCmd.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@paymentMethod",[Data.SQLDBType]::NVARCHAR))) | Out-Null
+    $SqlCmd.Parameters[4].Value = $paymentMethod;
+
+    $SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter 
+    $SqlAdapter.SelectCommand = $SqlCmd
+    $SqlAdapter.SelectCommand.CommandTimeout=360
+
+    $DataSet = New-Object System.Data.DataSet 
+    $SqlAdapter.Fill($DataSet) | Out-Null
+
+    $sqlConnection.Close();
+   ############################################################################################################################
+
+    $mainArchiveDate = (Get-Date).AddDays(-3)
+
+    foreach($sourceItem in $DataSet.Tables[0])
+    {
+        $source = $sourceItem.TH_POLICY_NUM
+                       
+        [datetime]$docDate = $sourceItem.TH_START_DATE
+            
+        $docYear = ($docDate).ToString('yyyy')
+        $docMonth = ($docDate).ToString('MM')
+        $docMonthName = (Get-Culture).DateTimeFormat.GetMonthName($docMonth)
+        $docDay = ($docDate).ToString('%d')
+        $dateTime  = ($docDate).ToString('ddMMyyyy')
+
+        if($docDate -gt $mainArchiveDate)
+        { 
+            $sourceFolder = $sourcePath+'\'+$product
+        }
+        else
+        {
+            $sourceFolder = '\\aafs01\theadarchive\mainArchive'+'\'+$product+'\'+$docYear+'\'+$docMonthName+'\'+$docDay
+        }
+
+        $sourceSearch = $source+'*'
+        $fileName = Get-ChildItem -Path $sourceFolder -Recurse | Where-Object {(!$_.PsIsContainer) -and ($_.Name -like $sourceSearch) -and ($_.Name -like $transType)} | Select-Object -First 1
+
+            
+        $destinationPublicFloder = $publicFolder+'\'+$docYear+'\'+$docMonthName+'\'+$product+'\'+$transaction
+        $destinationProdFloder = $prodFolder+'\'+$docYear+'\'+$docMonthName+'\'+$product+'\'+$transaction
+
+        $copySource = $sourceFolder+"\"+$fileName
+        $copyPublicDestination = $destinationPublicFloder+'\'+$product+'_'+$transaction+'_'+$paymentType+'_'+$dateTime+'.ps'
+        $copyProdDestination = $destinationProdFloder+'\'+$product+'_'+$transaction+'_'+$paymentType+'_'+$dateTime+'.ps'
+
+        if($copySource -eq $sourceFolder)
+        {
+            Write-Host "Could not find " $product+'_'+$transaction+'_'+$paymentType " in " $sourceFolder
+        }    
+        else
+        {
+            <#
+            ########COPY TO PUBLIC FOLDER##############
+            if (Test-Path $copyPublicDestination)
+            {
+                Write-Host "Copy of document already exists: " $copyPublicDestination
+            }
+            elseif (!(Test-Path $destinationPublicFloder))
+            {
+                New-Item -Path $destinationPublicFloder -ItemType "directory"
+                Copy-Item -Path $copySource -Destination $copyPublicDestination -Force
+
+                Write-Host "Copy of document retrieved from: " $copySource
+                Write-Host "Copy saved successfully in: " $copyPublicDestination
+            }
+            else
+            {
+                Copy-Item -Path $copySource -Destination $copyPublicDestination -Force
+
+                Write-Host "Copy of document retrieved from: " $copySource
+                Write-Host "Copy saved successfully in: " $copyPublicDestination
+            }
+            #>
+            ###########COPY TO PROD FOLDER##############
+            if (Test-Path $copyProdDestination)
+                {
+                    Write-Host "Copy of document already exists: " $copyProdDestination
+                }
+            elseif (!(Test-Path $destinationProdFloder))
+                {
+                    New-Item -Path $destinationProdFloder -ItemType "directory"
+                    Copy-Item -Path $copySource -Destination $copyProdDestination -Force
+
+                    Write-Host "Copy of document retrieved from: " $copySource
+                    Write-Host "Copy saved successfully in: " $copyProdDestination
+                }
+            else
+                {
+                    Copy-Item -Path $copySource -Destination $copyProdDestination -Force
+
+                    Write-Host "Copy of document retrieved from: " $copySource
+                    Write-Host "Copy saved successfully in: " $copyProdDestination
+                }
+         }
+     }
---
+++ latestDocCheck_Main.ps1
@@ -0,0 +1,54 @@
+    $startDate = (Get-Date -Day 1)#.AddMonths(-1)
+    #$startDate = Get-Date -Day 1 -Hour 0 -Minute 0 -Second 0 -Millisecond 0
+    $endDate = ($startDate).AddDays(15)
+    $publicFolder = 'I:\Document Snap Checks'
+    $prodFolder = 'D:\aascripts\ReportingScripts\docSnapCheck\test'
+    #$testFolder = 'I:\Document Snap Checks\TEST'
+
+$csvList1 = Import-Csv -Path 'D:\aascripts\ReportingScripts\docSnapCheck\latest_doc_check_list_Motor_Home_Mem.csv'
+foreach($oneItem in $csvList1)
+        {
+            $sourcePath = $oneItem.'sourcePath'
+            $batchName = $oneItem.'batchName'
+            $product = $oneItem.'product'
+            $transaction = $oneItem.'transaction'
+            $productType = $oneItem.'productType'
+            $paymentType = $oneItem.'paymentType'
+            $paymentMethod = $oneItem.'paymentMethod'
+            
+            Invoke-Expression "D:\aascripts\ReportingScripts\docSnapCheck\latestDocCheck_Motor_Home_Mem.ps1 $sourcePath $batchName $product $transaction $productType $paymentType $paymentMethod $startDate $endDate $publicFolder $prodFolder" -OutVariable out | Tee-Object -Variable out
+            
+        }
+
+$csvList2 = Import-Csv -Path 'D:\aascripts\ReportingScripts\docSnapCheck\latest_doc_check_list_Travel.csv'
+foreach($twoItem in $csvList2)
+        {
+            $sourcePath = $twoItem.'sourcePath'
+            $batchName = $twoItem.'batchName'
+            $product = $twoItem.'product'
+            $transaction = $twoItem.'transaction'
+            $productType = $twoItem.'productType'
+            $paymentType = $twoItem.'paymentType'
+            $paymentMethod = $twoItem.'paymentMethod'
+            $transType = $twoItem.'transType'
+                        
+            Invoke-Expression "D:\aascripts\ReportingScripts\docSnapCheck\latestDocCheck_Travel.ps1 $sourcePath $batchName $product $transaction $productType $paymentType $paymentMethod $transType $startDate $endDate $publicFolder $prodFolder" -OutVariable out | Tee-Object -Variable out
+        }
+
+$csvList3 = Import-Csv -Path 'D:\aascripts\ReportingScripts\docSnapCheck\latest_doc_check_list_HomeLegacy_HM.csv'
+foreach($threeItem in $csvList3)
+        {
+            $sourcePath = $threeItem.'sourcePath'
+            $batchName = $threeItem.'batchName'
+            $product = $threeItem.'product'
+            $transaction = $threeItem.'transaction'
+            $productType = $threeItem.'productType'
+            $paymentType = $threeItem.'paymentType'
+            $paymentMethod = $threeItem.'paymentMethod'
+            $transType = $threeItem.'transType'
+            
+            Invoke-Expression "D:\aascripts\ReportingScripts\docSnapCheck\latestDocCheck_Legacy_HM.ps1 $sourcePath $batchName $product $transaction $productType $paymentType $paymentMethod $transType $startDate $endDate $publicFolder $prodFolder" -OutVariable out | Tee-Object -Variable out
+            
+        }   
+
+ 
